b8cf3278770026fc3e553aefc480ca3348484afe
lailahr <lailah@AncestreStudio.local>
2016-05-20 14:02:10 +0200

    add trends in stockout line chart, basic chart styling in options
    uses google charts

--- js/control.js
+++ js/control.js
@@ -1,27 +1,23 @@
 
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Month', 'Percentage'],
+          ['January',  95.5],
+          ['February',  96.8],
+          ['March',  95.2],
+          ['April',  94.7]
+        ]);
 
+        var options = {
+          title: 'TRENDS IN MEDICINE STOCKOUTS',
+          curveType: 'none',
+          legend: { position: 'bottom' },
+          colors: ['#EC7F24'],
+          titleTextStyle: {color:'#EC7F24', fontSize: 20, fontName: 'Lato'},
+          tooltip: {textStyle: {color:'#CA640F'}}
+        };
 
+        var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
 
-// Charts
-google.charts.setOnLoadCallback(drawChart);
-function drawChart() {
-  var data = google.visualization.arrayToDataTable([
-    ['Month', 'Percentage'],
-    ['January', 95.4], ['February', 96.2], ['March', 95], ['April', 90]
- ]);
-
-  var options = {
-    title: 'Descendants by Generation',
-    hAxis: {title: 'Month'},
-    vAxis: {title: 'Percentage', minValue: 0, maxValue: 100},
-    trendlines: {
-      0: {
-        type: 'exponential',
-        visibleInLegend: true,
+        chart.draw(data, options);
       }
-    }
-  };
-
-  var chart = new google.visualization.ScatterChart(document.getElementById('chart_div'));
-  chart.draw(data, options);
-}
