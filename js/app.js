e6949bd089b4d2b940e5b31daf59ddca1e55d243
smmbll <lion@smmbll.com>
2016-08-19 17:00:20 +0200

    First iteration

--- js/app.js
+++ js/app.js
@@ -2,7 +2,32 @@
   var qs = document.querySelector.bind(document);
   var qsa = document.querySelectorAll.bind(document);
   var stockoutData = null;
-  var images = {
+  var constants = {};
+
+  // Cache the DOM for each province so that we don't have to
+  // remake it all every time a province is selected
+  var domCache = {};
+  var provinceIndex = {};
+
+  // DOM Elements
+  var DOM = {};
+
+  DOM.$stockTable = qs('#prv-stock-table tbody');
+  DOM.$totalClinics = qs('#clinics-total');
+  DOM.$prvBtns = qs('#prv-btns');
+  DOM.$prvName = qs('#prv-name');
+  DOM.$prvClinics = qs('#prv-clinics');
+  DOM.$prvImg = qs('#prv-image');
+  DOM.$prvAvail = qs('#prv-avail');
+  DOM.$availDate = qs('#avail-date');
+  DOM.$reportDD = qs('#report-dropdown');
+  DOM.$counter = qs('#counter');
+  DOM.$dropdown = qs('.dropdown');
+  DOM.$dropdownMenu = qs('.dropdown-menu');
+
+  // Set up constant values
+  constants.URL = 'result.json';
+  constants.PRV_IMAGES = {
     EC: 'ec.png',
     FS: 'fs.png',
     GT: 'gau.png',
@@ -13,133 +38,282 @@
     NW: 'nw.png',
     WC: 'wc.png'
   };
-  // Cache the DOM for each province so that we don't have to
-  // remake it all every time a province is selected
-  var domCache = {};
-  var provinceIndex = {};
+  constants.MONTHS = ['Jan','Feb','Mar','Apr','May','June','July','Aug','Sept','Oct','Nov','Dec'];
 
-  // DOM Containers
-  var $inStock = qs('#prv-in-stock tbody');
-  var $stockouts = qs('#prv-stockouts tbody');
-  var $btns = qsa('.prv-btn');
-  var $clinicsTotal = qs('#clinics-total');
-  var $prvClinics = qs('#prv-clinics');
-  var $prvName = qs('#prv-name');
-  var $prvImg = qs('#prv-image');
+  // Setup dropdown
+  window.addEventListener('click', function(e) {
+    var $target = e.target;
+
+    if($target.classList.contains('dropdown-toggle')) {
+      DOM.$dropdownMenu
+        .classList
+        .toggle('show');
+    } else {
+      DOM.$dropdownMenu
+        .classList
+        .remove('show');
+    }
+  });
 
   // Pull in data
-  fetch('result.json')
+  fetch(constants.URL)
     .then(function(res) {
         if(!res.ok) console.log('Error with response.');
         else {
           return res.json()
           .then(function(data) {
+            // Set data
             stockoutData = data;
 
+            // Sort provinces by name
+            stockoutData.provinces.sort(function(a,b) {
+              return a.name > b.name;
+            });
+
             // Update total number of clinics
-            $clinicsTotal.innerHTML = stockoutData.total_clinics;
+            view({ target: 'totalClinics', change: stockoutData.country_stats.total_clinics });
 
-            // Create a list of indexes that point us to where each ID is
-            // in the the list of province objects so that we don't have to
-            // search the array every time we want to find a province
-            stockoutData.provinces.forEach(function(province,i) {
-              provinceIndex[province.code] = i;
-            });
-
-            displayProvinceData('EC');
-
-            // Event listener should only be attached once we've
-            // gotten the data we need
-            [].forEach.call($btns, function($btn) {
-              $btn.addEventListener('click', function(e) {
-                var $target = e.target;
-                var code = $target.getAttribute('data-code');
-                var classes = $target.classList;
-
-                // Toggle active button classes
-                [].forEach.call($btns, function($btn) {
-                  $btn.classList.remove('active');
-                });
-
-                $target.classList.add('active');
-
-                // Show province data
-                if(code) {
-                  displayProvinceData(code);
-                } else {
-                  console.log('Province data does not exist for province with code ' + code);
-                }
-              });
-            });
+            calculateCounter();
+            setupButtons(stockoutData.provinces);
           });
         }
       })
     .catch(function(err) {
-      console.log('Request errored with following message: ' + err.message);
+      console.log(Error(err));
     });
 
+  function isArray(arr) {
+    if(!Array.isArray) {
+      return Object.prototype.toString.call( arr ) === '[object Array]';
+    } else {
+      return Array.isArray(arr);
+    }
+  }
+
+  function calculateCounter() {
+    var now = new Date();
+    var toEndOfYear = ((new Date(new Date().getFullYear(), 11, 31) - now) * 100) / now;
+    var c = 2 * 80 * Math.PI;
+    var strokeLength = c - (toEndOfYear * c);
+
+    view({ target: 'counter', change: strokeLength, type: 'dash' });
+  }
+
+  function setupButtons(provinces) {
+    // Create a list of indexes that point us to where each ID is
+    // in the the list of province objects so that we don't have to
+    // search the array every time we want to find a province
+    provinces.forEach(function(province,i) {
+      var code = province.code;
+
+      provinceIndex[code] = i;
+
+      createButton(province.name,code,code === 'EC');
+    });
+
+    // Add buttons to DOM object
+    DOM.$btns = qsa('.prv-btn');
+
+    // Attach click handlers and display province data as callback
+    buttonClickHandlers(displayProvinceData.bind(null,'EC'));
+  }
+
+  function createButton(name,code,active) {
+    var dom = {};
+    var listItem = document.createElement('li');
+    var btn = document.createElement('button');
+
+    listItem.classList.add('list-item');
+
+    btn.innerHTML = name;
+    btn.setAttribute('type','button');
+    btn.setAttribute('data-code',code);
+    btn.classList.add('btn','btn-default','prv-btn');
+    if(active) btn.classList.add('active');
+
+    listItem.appendChild(btn);
+
+    dom.target = 'prvBtns';
+    dom.change = listItem;
+    dom.type = 'append';
+
+    view(dom);
+  }
+
+  function buttonClickHandlers(cb) {
+    // Event listener should only be attached once we've
+    // gotten the data we need
+    var $btns = DOM.$btns;
+
+    [].forEach.call($btns, function($btn) {
+      $btn.addEventListener('click', function(e) {
+        var $target = e.target;
+        var code = $target.getAttribute('data-code');
+        var classes = $target.classList;
+
+        // Toggle active button classes
+        [].forEach.call($btns, function($btn) {
+          $btn.classList.remove('active');
+        });
+
+        $target.classList.add('active');
+
+        // Show province data
+        if(code) {
+          displayProvinceData(code);
+        } else {
+          console.log('Province data does not exist for province with code ' + code);
+        }
+      });
+    });
+
+    if(cb) {
+      cb();
+    }
+  }
+
   // Generate a row in the medicines table
-  function medicineRow(medicine) {
+  function medicineRow(row) {
+    var medicine = row.name;
     var stringSplit = medicine.split('(');
     var name = stringSplit[0] ? stringSplit[0] : '';
     var dosage = stringSplit[1] ? '(' + stringSplit[1] : '';
 
-    return '<tr><td>' + name + '<span class="dosage">' + dosage + '</span></td></tr>';
+    return '<tr class="' + row.status + '"><td>' + name + '<span class="dosage">' + dosage + '</span></td><td class="avail text-center"><h3>' + row.avail + '%</h3></td><td class="total text-center"><h4><strong>' + row.totalChecks + '</strong></h4></td></tr>';
   }
 
   // Clear DOM
   function clear() {
-    $inStock.innerHTML = '';
-    $stockouts.innerHTML = '';
+    for(var key in DOM) {
+      DOM[key].innerHtml = '';
+    }
+  }
+
+  // Create table of row objects showing availability of each medicine
+  function availabilityTable(stats) {
+    var medsInStock = stats.medicine_instock;
+    var medsOutOfStock = stats.medicine_stockouts;
+    var medicines = stockoutData.medicines;
+    var table = [];
+
+    for(var med in medicines) {
+      var inStockChecks = medsInStock[med];
+
+      if(!!inStockChecks) {
+        var row = {};
+        var outOfStockChecks = medsOutOfStock[med] ? medsOutOfStock[med] : 0;
+        var totalChecks = inStockChecks + outOfStockChecks;
+        var availability = (inStockChecks / totalChecks * 100).toFixed(0);
+
+        row.name = medicines[med];
+        row.totalChecks = totalChecks;
+        row.avail = availability
+
+        if(availability <= 50) {
+          row.status = 'critical';
+        } else if(availability > 50 && availability <= 75) {
+          row.status = 'intermediate';
+        } else {
+          row.status = 'normal';
+        }
+
+        table.push(row);
+      }
+    }
+
+    table.sort(function(a,b) {
+      return a.avail - b.avail;
+    });
+
+    return table;
+  }
+
+  function view(dom) {
+    if(!!dom) {
+      if(isArray(dom)) {
+        dom.forEach(function(el) {
+          var domTarget = DOM['$' + el.target];
+          var domChange = el.change;
+          var type = el.type ? el.type : 'set'; // Default to replacing old content with new
+
+          switch(type) {
+            case 'set':
+              domTarget.innerHTML = domChange;
+              break;
+            case 'append':
+              domTarget.appendChild(domChange);
+              break;
+            case 'img':
+              domTarget.setAttribute('src','img/' + domChange);
+              break;
+            case 'dash':
+              domTarget.setAttribute('stroke-dashoffset',domChange);
+              break;
+          }
+        });
+      } else if(!isArray(dom) && typeof dom === 'object') {
+        view([dom]);
+      }
+    } else {
+      console.log('No keys supplied to view, so DOM remains unchanged.');
+    }
   }
 
   // Insert the correct DOM
   function displayProvinceData(code) {
     var province = stockoutData.provinces[provinceIndex[code]];
+    var stockData;
 
     if(!!province) {
-      var inStock = '';
-      var stockouts = '';
-      var clinics = {};
       var provinceCache = domCache[code];
+      var stats = province.stats;
+      var stockTable = '';
+      var stockData = null;
+      var dom = [];
 
-      if(!!provinceCache) {
-        clinics = provinceCache.clinics;
-        inStock = provinceCache.inStock;
-        stockouts = provinceCache.stockouts;
-      } else {
-        var stats = province.stats;
-        var medsInStock = stats.medicine_instock;
-        var medsStockouts = stats.medicine_stockouts;
-        var medicineNames = stockoutData.medicines;
+      // Get most recent monthly availability
+      var monthlyAvail = stats.monthly_availability;
+      var latestDate = null;
+      var dateFormat = '';
+      var availability = null;
 
-        // Provincial clinics
-        $prvName.innerHTML = province.name;
-        $prvClinics.innerHTML = 5;
-        $prvImg.setAttribute('src','img/' + images[code]);
+      for(var key in monthlyAvail) {
+        var currentAvail = monthlyAvail[key];
+        var currentDate = new Date(key);
 
-        // In Stock
-        for(var med in medsInStock) {
-          inStock = inStock += medicineRow(medicineNames[med]);
+        if(currentDate > latestDate) {
+          latestDate = currentDate;
+          availability = currentAvail.toFixed(0);
         }
-
-        // Stockouts
-        for(var med in medsStockouts) {
-          stockouts = stockouts += medicineRow(medicineNames[med]);
-        }
-
-        // Add these to the cache
-        var provinceCache = {};
-
-        provinceCache.inStock = inStock;
-        provinceCache.stockouts = stockouts;
-        provinceCache.clinics = clinics;
-        domCache[code] = provinceCache;
       }
 
-      // Push these to the DOM
-      $inStock.innerHTML = inStock;
-      $stockouts.innerHTML = stockouts;
+      dateFormat = constants.MONTHS[latestDate.getMonth()];
+
+      if(!!provinceCache) {
+        stockData = provinceCache;
+      } else {
+        stockData = availabilityTable(province.stats);
+
+        // Add these to the cache
+        domCache[code] = stockData;
+      }
+
+      // Set up stock table row elements
+      stockData.forEach(function(row) {
+        stockTable += medicineRow(row);
+      });
+
+      // Create new DOM
+      dom.push({ target: 'prvName', change: province.name });
+      dom.push({ target: 'prvClinics', change: stats.total_clinics });
+      dom.push({ target: 'prvImg', change: constants.PRV_IMAGES[code], type: 'img' });
+      dom.push({ target: 'stockTable', change: stockTable });
+      dom.push({ target: 'prvAvail', change: availability + '%' });
+      dom.push({ target: 'availDate', change: dateFormat });
+
+      // Set the view
+      view(dom);
+
     } else {
       console.log('No data for province code ' + code);
       clear();
--- js/control.js
+++ js/control.js
@@ -1,28 +1,28 @@
-      function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-          ['Month', 'Percentage'],
-          ['January', 95.5],
-          ['February', 96.8],
-          ['March', 95.2],
-          ['April', 94.7]
-        ]);
+function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+    ['Month', 'Percentage'],
+    ['January', 95.5],
+    ['February', 96.8],
+    ['March', 95.2],
+    ['April', 94.7]
+  ]);
 
-        var options = {
-          curveType: 'none',
-          legend: { position: 'none' },
-          colors: ['#EC7F24'],
-          chartArea: {'width': '80%', 'height': '80%'},          
-          titleTextStyle: {color:'#333', fontSize: 24, fontName: 'Lato'},
-          tooltip: {textStyle: {color:'#333'}},
-          vAxis: {
-                format: '#\'%\'',
-            },          
-        };
+  var options = {
+    curveType: 'none',
+    legend: { position: 'none' },
+    colors: ['#EC7F24'],
+    chartArea: {'width': '80%', 'height': '80%'},
+    titleTextStyle: {color:'#333', fontSize: 24, fontName: 'Lato'},
+    tooltip: {textStyle: {color:'#333'}},
+    vAxis: {
+          format: '#\'%\'',
+      },
+  };
 
-        var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+  var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
 
-        chart.draw(data, options);
-      };  
+  chart.draw(data, options);
+};
 
 function provinceClinicsTotal(number) {
   document.getElementById("prv-cli-total").innerHTML = number + " <small>since 2014</small>";
