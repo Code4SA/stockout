e3ce82cb3205632b299575296c5fea80c23a0d8e
smmbll <lion@smmbll.com>
2016-08-30 15:15:49 +0200

    Changed prv-btns layout

--- js/embed.js
+++ js/embed.js
@@ -4,7 +4,7 @@ if (document.location.hostname == "localhost") {
     var baseurl = "https://static.code4sa.org/stockout/";
 }
 document.write('<div id="code4sa-embed-stockout"></div>');
-document.write('<script type="text/javascript" src="' + baseurl + 'js/pym.js"></script>');
+document.write('<script type="text/javascript" src="' + baseurl + 'js/vendor/pym.js"></script>');
 document.write("<script>\n" +
 "var pymParent = new pym.Parent('code4sa-embed-stockout', '" + baseurl + "index.html?show-embed-link=true', {});\n" +
 "</script>");
